57d95398496854923621bec7a9991b5578f90180
PaulMarsh-UoL <pm286@student.london.ac.uk>
2024-08-15 22:49:32 -0700

    PRJ-29: Initial commit

--- src/routes/user.js
+++ src/routes/user.js
@@ -113,6 +113,76 @@ router.get('/dashboard', async (req, res) => {
     });
 });
 
+/**
+ * @desc Renders the income page
+ */
+router.get('/income', async (req, res) => {
+    // redirect user to login if no session is found
+    if (!req.session.userId) {
+        console.log('No user ID found in session, redirecting to login'); // Debug log
+        return res.redirect('/account/login');
+    }
+
+    // save user's ID
+    const userId = req.session.userId;
+
+    const db = await open({
+        filename: "./database.db",
+        driver: sqlite3.Database,
+    });
+
+    // create query to retrieve user's total income
+    const totalIncomeQuery = `SELECT COALESCE(SUM(amount), 0) AS totalIncome FROM income WHERE user_id = ? AND substr(date, 1, 4) = ? AND substr(date, 6, 2) = ?`;
+
+    // create query to retrieve user's budgeted income
+    const budgetedIncomeQuery = `SELECT COALESCE(SUM(amount), 0) AS budgetedIncome FROM incomeBudget WHERE user_id = ?`;
+
+    // create query to retrieve user's recent transactions
+    const incomesQuery = `
+    SELECT * FROM (
+    SELECT 'income' AS type, source, amount, date, IC.name AS category, IC.icon AS icon
+    FROM income I
+    JOIN incomeCategory IC ON I.income_category_id = IC.income_category_id
+    WHERE I.user_id = ?
+    )
+    ORDER BY date DESC`;
+
+    let incomes;
+    let totalIncome;
+    let budgetedIncome;
+
+    let most_recent_date = new Date();
+    let year = most_recent_date.toLocaleString("en-US", { year: "numeric" });
+
+    try {
+        incomes = await db.all(incomesQuery, [userId]);
+        // Determine which month we're in by looking at most recent transaction date
+        if (incomes) {
+            most_recent_date = new Date(incomes[0].date);
+        }
+        const paddedMonth = (most_recent_date.getMonth() + 1).toString().padStart(2, "0");
+        year = most_recent_date.toLocaleString("en-US", { year: "numeric" })
+
+        totalIncome = (await db.get(totalIncomeQuery, [userId, year, paddedMonth])).totalIncome;
+        budgetedIncome = (await db.get(budgetedIncomeQuery, [userId])).budgetedIncome;
+    } catch (error) {
+        console.error(error.message);
+        return res.sendStatus(500);
+    }
+
+    const month = most_recent_date.toLocaleString("en-US", { month: "long" });
+
+    // if there are no errors, load the user's dashboard
+    res.render('income', {
+        month,
+        year,
+        totalIncome,
+        budgetedIncome,
+        incomes
+    });
+});
+
+
 // /**
 //  * @desc Adds a new income record
 //  */

b855314053b94a14495fa916b83a34795af9375f
Tanyi <tanyitk@gmail.com>
2024-08-17 16:32:02 -0400

    update navbar from expense to expenses

--- src/public/scripts/navbar.js
+++ src/public/scripts/navbar.js
@@ -19,7 +19,7 @@ transitionEndedEvent = getTransitionEndedEventName();
 const pages = [
     "/user/dashboard",
     "/user/income",
-    "/user/expense",
+    "/user/expenses",
     "/user/summary"
 ];
 
@@ -36,12 +36,12 @@ const callbacks = [
     () => onTransitionEnded(3),
 ];
 
-const pages = [
-    "/user/dashboard",
-    "/user/income",
-    "/user/expenses",
-    "/user/summary"
-];
+// const pages = [
+//     "/user/dashboard",
+//     "/user/income",
+//     "/user/expenses",
+//     "/user/summary"
+// ];
 
 const onTransitionEnded = (index) => {
     button = selected_button = document.querySelector("div#navbar div.navbar-button.selected");
